156b386a60cee7c8dd3ae58b88b1f2d4b96c5214
Briii <brianwkhuffman@gmail.com>
2019-09-11 20:06:37 -1000

    Finished

--- palindromic-number-generator.js
+++ palindromic-number-generator.js
@@ -1,5 +1,17 @@
-function palindromicNumberGenerator(num) {
-  // do work here
+function palindromicNumberGenerator(num, steps) {
+  // do work here]
+  steps = steps || 0;
+  let value = num;
+  let reverseNum = parseInt(num.toString().split('').reverse().join(''));
+  if (num !== reverseNum) {
+    steps++;
+    value += reverseNum;
+    return palindromicNumberGenerator(value, steps);
+  }
+  return {
+    steps: steps,
+    value: num
+  };
 }
 
 module.exports = palindromicNumberGenerator;
